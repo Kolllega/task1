886d11aff9ca0ad78d2b320966f170adf113e919
kolllega <k_timka@list.ru>
2022-10-07 06:12:15 +0300

    3 task

---
+++ task1/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число :");
+string num = Console.ReadLine();
+int number = int.Parse(num);
+Console.WriteLine("Все четные числа от 1 до введенного числа");
+if(number % 2 == 0)
+    
+    for (int i = 2; i <= number; i++)
+    {
+        Console.WriteLine($"{i}");i++;
+    }
+else   
+    
+    for (int i = 2; i < number; i++)
+    {
+       Console.WriteLine($":{i}");i++;
+    }
+        
+    
