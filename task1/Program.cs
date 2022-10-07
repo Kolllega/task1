a5d6d27580dd3e1c6719f1724f2c4d7b80936daa
kolllega <k_timka@list.ru>
2022-10-07 06:01:22 +0300

    TAsk1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число 1 :");
+string number1 = Console.ReadLine();
+Console.WriteLine("Введите число 2 :");
+string number2 = Console.ReadLine();
+
+int numberOne = int.Parse(number1);
+int numberTwo = int.Parse(number2);
+
+if (numberOne > numberTwo)
+
+        Console.WriteLine($"max = {numberOne}");
+
+  else
+
+        Console.WriteLine($"max = {numberTwo}");
